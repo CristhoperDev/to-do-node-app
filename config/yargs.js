1ea7fb002abed6e5f44d2b2d8f1f1ed18b43e744
Cristhoper Alvarez <acristhoper25@gmail.com>
2020-04-20 18:59:58 -0500

    optimization

--- config/yargs.js
+++ config/yargs.js
@@ -1,29 +1,25 @@
+const description = {
+    demand: true,
+    alias: 'd',
+    description: 'Task description to do'
+};
+
+const completed = {
+    alias: 'c',
+    default: true,
+    description: 'Mark task as completed or pending'
+};
+
 const argv = require('yargs')
             .command('create', 'Create task to do', {
-                description: {
-                    demand: true,
-                    alias: 'd',
-                    description: 'Task description to do'
-                },
+                description
             })
             .command('update', 'Update task to do', {
-                description: {
-                    demand: true,
-                    alias: 'd',
-                    description: 'Task description to do'
-                },
-                completed: {
-                    alias: 'c',
-                    default: true,
-                    description: 'Mark task as completed or pending'
-                }
+                description,
+                completed
             })
             .command('delete', 'delete task to do', {
-                description: {
-                    demand: true,
-                    alias: 'd',
-                    description: 'Task description to do'
-                },
+                description
             })
             .help()
             .argv;
